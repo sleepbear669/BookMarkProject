1b12c7baf0db02ac2fffbce5ba9ac8ebe8663d58
sleepbear <sleepbear669@gmail.com>
2015-11-30 21:59:14 +0900

    [Config] maven dependency, gitignore

---
+++ src/main/java/gom/cave/sleep/bookmark/Application.java
@@ -0,0 +1,16 @@
+package gom.cave.sleep.bookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sleepbear on 2015. 11. 30..
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/gom/cave/sleep/bookmark/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by sleepbear on 2015. 11. 30..
+ */
+package gom.cave.sleep.bookmark;
