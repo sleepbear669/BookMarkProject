647b8f6d2c2b957ac562d98398b1b4db7193bf78
sleepbear <sleepbear669@gmail.com>
2015-12-02 00:11:00 +0900

    [Make] get , add , delete by email,

---
+++ src/main/java/gom/cave/sleep/bookmark/repository/MemberRepository.java
@@ -0,0 +1,58 @@
+package gom.cave.sleep.bookmark.repository;
+
+import gom.cave.sleep.bookmark.model.Member;
+
+import java.sql.*;
+
+/**
+ * Created by sleepbear on 2015. 12. 1..
+ */
+public class MemberRepository {
+    public void add(Member member) throws ClassNotFoundException, SQLException {
+
+        final Connection connection = getConnection();
+        final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO member(email, password, nickname) VALUES (?,?,?)");
+        preparedStatement.setString(1, member.getEmail());
+        preparedStatement.setString(2, member.getPassword());
+        preparedStatement.setString(3, member.getNickname());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public Member get(String email) throws SQLException, ClassNotFoundException {
+        final Connection connection = getConnection();
+        final PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from member where email = ?");
+        preparedStatement.setString(1, email);
+
+        final ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        final long id = resultSet.getLong("id");
+        final String email1 = resultSet.getString("email");
+        final String password = resultSet.getString("password");
+        final String nickname = resultSet.getString("nickname");
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return new Member(id, email1, password, nickname);
+    }
+
+    public void delete(String email) throws SQLException, ClassNotFoundException {
+        final Connection connection = getConnection();
+        final PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM member WHERE email=?");
+        preparedStatement.setString(1, email);
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/bookmark_sharing", "bookmark", "gom0119!1");
+    }
+}
